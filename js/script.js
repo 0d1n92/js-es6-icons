da0aa115e8e4353fa7269137c0432e980a1d9748
enrico <rombaldonienrico@gmail.com>
2020-12-03 16:11:46 +0100

    print icons

---
+++ js/script.js
@@ -0,0 +1,135 @@
+// milestone 1:
+// definire un array di oggetti; ogni oggetto
+// rappresenta un'icona, che è caratterizzata da:
+// nome, prefisso, tipo e famiglia.
+// Utilizzando la funzione forEach e il template
+// literal, visualizzare in pagina tutte le icone con il
+// proprio nome.
+$(document).ready(
+  function(){
+const icons = [
+      {
+        name: "dog",
+        prefix: "fa-",
+        type: "animal",
+        family: "fas"
+      },
+      {
+        name: "fish",
+        prefix: "fa-",
+        type: "animal",
+        family: "fas"
+      },
+      {
+        name: "tractor",
+        prefix: "fa-",
+        type: "vehicle",
+        family: "fas"
+      },
+      {
+        name: "hippo",
+        prefix: "fa-",
+        type: "animal",
+        family: "fas"
+      },
+      {
+        name: "spider",
+        prefix: "fa-",
+        type: "animal",
+        family: "fas"
+      },
+      {
+        name: "apple-alt",
+        prefix: "fa-",
+        type: "vegetable",
+        family: "fas"
+      },
+      {
+        name: "carrot",
+        prefix: "fa-",
+        type: "vegetable",
+        family: "fas"
+      },
+      {
+        name: "lemon",
+        prefix: "fa-",
+        type: "vegetable",
+        family: "fas"
+      },
+      {
+        name: "dragon",
+        prefix: "fa-",
+        type: "animal",
+        family: "fas"
+      },
+      {
+        name: "pepper-hot",
+        prefix: "fa-",
+        type: "vegetable",
+        family: "fas"
+      },
+      {
+        name: "space-shuttle",
+        prefix: "fa-",
+        type: "vehicle",
+        family: "fas"
+      },
+      {
+        name: "truck-monster",
+        prefix: "fa-",
+        type: "vehicle",
+        family: "fas"
+      },
+      {
+        name: "wheelchair",
+        prefix: "fa-",
+        type: "vehicle",
+        family: "fas"
+      },
+      {
+        name: "tram",
+        prefix: "fa-",
+        type: "vehicle",
+        family: "fas"
+      },
+      {
+        name: "fighter-jet",
+        prefix: "fa-",
+        type: "vehicle",
+        family: "fas"
+      },
+      {
+        name: "helicopter",
+        prefix: "fa-",
+        type: "vehicle",
+        family: "fas"
+      }
+    ];
+    const container= $("#container");
+     print(icons,container);
+  });
+
+
+
+  // Utilizzando la funzione forEach e il template
+  // literal, visualizzare in pagina tutte le icone con il
+  // proprio nome.
+function print(array,container){
+
+  array.forEach((element) => {
+    const { family,prefix,name }= element;
+    container.append(
+      `
+      <div>
+      <i class="${family} ${prefix}${name}"></i>
+      </div>
+
+
+      `
+
+    )
+
+  });
+
+
+}
